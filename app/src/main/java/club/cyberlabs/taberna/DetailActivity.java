d97b02f2e040c0734edf69ccb18a781f2ca9f790
Himanshu <hg24091996@gmail.com>
2017-01-21 10:42:25 +0530

    hg detail activity

--- app/src/main/java/club/cyberlabs/taberna/DetailActivity.java
+++ app/src/main/java/club/cyberlabs/taberna/DetailActivity.java
@@ -1,13 +1,62 @@
 package club.cyberlabs.taberna;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
 
 public class DetailActivity extends AppCompatActivity {
 
+    TextView title,sub,price;
+    ImageView image;
+    JSONObject jsonObject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        title=(TextView)findViewById(R.id.detail_title);
+        sub=(TextView)findViewById(R.id.detail_sub);
+        price=(TextView)findViewById(R.id.detail_price);
+        image=(ImageView)findViewById(R.id.detail_image);
+        try {
+            jsonObject = new JSONObject(getIntent().getStringExtra("json"));
+            new DownloadImageTask(image)
+                    .execute(jsonObject.getString("image"));
+            title.setText(jsonObject.getString("title"));
+            sub.setText(jsonObject.getString("subtitle"));
+
+        }catch (JSONException j){}
+
+    }
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
