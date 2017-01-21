640de64e9280fc589c9aaf1015930439f9646db6
Himanshu <hg24091996@gmail.com>
2017-01-21 23:56:56 +0530

    hg catalogue activity

--- app/src/main/java/club/cyberlabs/taberna/MainActivity.java
+++ app/src/main/java/club/cyberlabs/taberna/MainActivity.java
@@ -20,9 +20,48 @@ public class MainActivity extends AppCompatActivity {
         editText.clearFocus();
     }
 
-    public void showCatalogue(View view)
+
+    public void Gadgets(View view)
     {
-        startActivity(new Intent(this,CatalogueActivity.class));
+        Intent intent = new Intent(getApplicationContext(),CatalogueActivity.class);
+        intent.putExtra("ans",0);
+        startActivity(intent);
+    }
+    public void Appliances(View view)
+    {
+        Intent intent1 = new Intent(getApplicationContext(),CatalogueActivity.class);
+        intent1.putExtra("ans",1);
+        startActivity(intent1);
+    }
+    public void Books(View view)
+    {
+        Intent intent2 = new Intent(getApplicationContext(),CatalogueActivity.class);
+        intent2.putExtra("ans",2);
+        startActivity(intent2);
+    }
+    public void Toys(View view)
+    {
+        Intent intent3 = new Intent(getApplicationContext(),CatalogueActivity.class);
+        intent3.putExtra("ans",3);
+        startActivity(intent3);
+    }
+    public void Stationery(View view)
+    {
+        Intent intent4 = new Intent(getApplicationContext(),CatalogueActivity.class);
+        intent4.putExtra("ans",4);
+        startActivity(intent4);
+    }
+    public void Medicine(View view)
+    {
+        Intent intent5 = new Intent(getApplicationContext(),CatalogueActivity.class);
+        intent5.putExtra("ans",5);
+        startActivity(intent5);
+    }
+    public void DailyUse(View view)
+    {
+        Intent intent6 = new Intent(getApplicationContext(),CatalogueActivity.class);
+        intent6.putExtra("ans",6);
+        startActivity(intent6);
     }
 
     public void onSearch(View view)
---
+++ app/src/main/java/club/cyberlabs/taberna/TwoFragment.java
@@ -0,0 +1,34 @@
+package club.cyberlabs.taberna;
+
+/**
+ * Created by Himanshu on 21-01-2017.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Aman on 21-01-2017.
+ */
+
+public class TwoFragment extends Fragment {
+
+    public TwoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+}
